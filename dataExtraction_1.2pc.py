fab4b15d4e19839ceed37518e8029d71cd20cac0
Tiggger <johnnyalexjw@gmail.com>
2025-08-06 15:05:34 +0100

    Added interactive s ordering heatmap.

--- dataExtraction_1.2pc.py
+++ dataExtraction_1.2pc.py
@@ -8,66 +8,45 @@ import imageProcessingTool as dc
 import matplotlib.pyplot as plt
 from PIL import Image
 
-#image_dir='/Volumes/SANDISK/images/nikon3/28.07.25/1.2%/2025-07-28_singleCellResolving_20xwithExtender_BF_featureImages_mixofExposuretimes_exports/2025-07-28_singleCellResolving+20xwithExtender_BF_featureImages_mixofExposuretimes_MMStack_Default.ome.jpg'
-#xsplit = 6
-#ysplit = 6
-#kernelSize = 4
-#threshold = 128
+#image_dir='/Users/johnwhitfield/Desktop/proper/t:19:21 - 2025-07-28_singleCellResolving+20xwithExtender_BF_.jpg'
 
-#image_dir='/Volumes/SANDISK/images/nikon3/28.07.25/1.2%/2025-07-28_singleCellResolving_20xwithExtender_BF_featureImages_mixofExposuretimes_exports/2025-07-28_singleCellResolving+20xwithExtender_BF_featureImages_mixofExposuretimes_MMStack_Default.ome2.jpg'
-#xsplit = 6
-#ysplit = 6
-#kernelSize=6
-#threshold=110
+image_dir='/Users/johnwhitfield/Desktop/proper/t:20:21 - 2025-07-28_singleCellResolving+20xwithExtender_BF_.jpg'
 
-#image_dir='/Volumes/SANDISK/images/nikon3/28.07.25/1.2%/2025-07-28_singleCellResolving_20xwithExtender_BF_featureImages_mixofExposuretimes_exports/2025-07-28_singleCellResolving+20xwithExtender_BF_featureImages_mixofExposuretimes_MMStack_Default.ome4.jpg'
-#xsplit = 6
-#ysplit = 6
-#kernelSize=8
-#threshold=90
-
-#image_dir='/Volumes/SANDISK/images/nikon3/28.07.25/1.2%/2025-07-28_singleCellResolving_20xwithExtender_BF_featureImages_mixofExposuretimes_exports/2025-07-28_singleCellResolving+20xwithExtender_BF_featureImages_mixofExposuretimes_MMStack_Default.ome5.jpg'
-#xsplit = 6
-#ysplit = 6
-#kernelSize=4
-#threshold=150
-
-#image_dir='/Volumes/SANDISK/images/nikon3/28.07.25/1.2%/2025-07-28_singleCellResolving_20xwithExtender_BF_featureImages_mixofExposuretimes_exports/2025-07-28_singleCellResolving+20xwithExtender_BF_featureImages_mixofExposuretimes_MMStack_Default.ome6.jpg'
-#xsplit = 6
-#ysplit = 6
-#kernelSize=4 #6 works also
-#threshold=135
-
-#tile image - ph1, doesn't look brilliant due to difference in cell colour between neigbouring cells
-#image_dir='/Volumes/SANDISK/images/nikon3/28.07.25/1.2%/2025-07-25_20xwithExtender_Ph1_singleCell_tileImages_25pcOverlap_1_processed/2025-07-25_20xwithExtender_Ph1_singleCell_tileImages_25pcOverlap_1_timepoint_0.jpg'
-#xsplit = 12
-#ysplit = 12
-#kernelSize=4
-#threshold=150
-
-#200 works suprisingly well
-image_dir='/Users/johnwhitfield/Desktop/testingLevelChanges.jpg'
-xsplit=7
-ysplit=7
-kernelSize=8
-threshold=200
+xsplit=5
+ysplit=5
+kernelSize=4
+threshold=150
 
 
+#creating synthetic data
+#L, d, N = 200, 10, 10
+#image_domain, angles_domain = dc.generate_nematic(L, d, N, mode='domain', domain_size=5)
+#img=image_domain
+
+#temp_path='/tmp/randomImage.tif'
+#Image.fromarray(img).save(temp_path)
+#a=mc.ImageAnalysis(temp_path, None, 4, kernelSize, threshold)
+
+
+#print('loading image')
 img = np.array(Image.open(image_dir))
 
+print('splitting into normal cells')
 cells = dc.splitIntoCells(img, xsplit, ysplit)
 
 
 
 a=mc.ImageAnalysis(image_dir, None, 4, kernelSize, threshold)
 
-densityCells = dc.splitIntoCells(a.binary_Image, xsplit, ysplit)
+plt.imshow(img, cmap='gray')
 
-binaryDensities=dc.calculateDensity(densityCells, binaryImage=True)
+#qtensor ordering
+print('calculating q ordering')
+qtensorsInfo=dc.calculateQTensor(cells, kernelSize, threshold)
 
-#plt.imshow(img, cmap='gray')
-plt.imshow(a.processed_png)
+print('making q map')
+#fig, ax = dc.create_nematicOrderingTensor_heatmap(img, cells, qtensorsInfo) a.masked
+dc.create_nematicOrderingTensor_heatmap_interactive(a.masked, cells, qtensorsInfo, kernelSize, threshold, 20, coarsening=1000)
 
-fig, ax = dc.create_interactive_heatmap(a.masked, cells, binaryDensities, kernelSize, threshold, 20)
 
 plt.show()
