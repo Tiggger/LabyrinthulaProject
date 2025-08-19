d050e794f879b67c36129815e454081773ac9d15
Tiggger <johnnyalexjw@gmail.com>
2025-08-19 09:19:38 +0100

    Cleaned up example use code and created a file for how to use synthetic data generation.

--- exampleUseSyntheticData.py
+++ exampleUseSyntheticData.py
@@ -8,25 +8,27 @@ import imageProcessingTool as dc
 import matplotlib.pyplot as plt
 from PIL import Image
 
-#import image - download image from repo and change path as needed
-image_dir='/Users/johnwhitfield/Desktop/projectSeagrass/desktopFiles/reprocessing/20x/t:5:12 - 2025-08-12_20x_BF_featureImages_MMStack_Pos0.jpg'
+#analysis parameters
+#seem to be pretty good settings
+xsplit=5*3
+ysplit=5*3
+kernelSize=4 
 
-#setting dimensions of grid
-xsplit=5
-ysplit=5
-#setting the size of the gaussian kernel
-kernelSize=4
-pixelSize=0.55
+pixelSize=0.5 #arbitrary value
 
 
-#load in the image
-img = np.array(Image.open(image_dir))
+#create synthetic data
+L, d, N = 1000, 40, 40
+image_domain, angles_domain = dc.generate_nematic(L, d, N, mode='smooth', domain_size=5)
+img=image_domain
+
+#create synthetic data temporary image path (needed as image analysis needs path to image to be used)
+temp_path='/tmp/randomImage.tif'
+Image.fromarray(img).save(temp_path)
+a=mc.ImageAnalysis(temp_path, None, 4, kernelSize)
 
 #split the image into cells
 cells = dc.splitIntoCells(img, xsplit, ysplit)
-
-#create a masterClass object
-a=mc.ImageAnalysis(image_dir, None, 4, kernelSize)
 
 #showing the image we pass in and the skeletonised image that has been created in the pipeline
 fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(8, 4), sharex=True, sharey=True)
@@ -41,12 +43,12 @@ ax[1].imshow(a.skeletonImage, cmap=plt.cm.gray)
 ax[1].axis('off')
 ax[1].set_title('Skeleton', fontsize=20)
 
-#calculate ordering from q tensor
-qtensorsInfo=dc.calculateQTensor(cells, kernelSize)
-
-#create the interactive orderig heatmap
-#the image for the heatmap can be changed, the masked image from the masterClass object is most useful, but a.rgb (angle map) can be useful to see too
-dc.create_nematicOrderingTensor_heatmap_interactive(a.masked, cells, qtensorsInfo, kernelSize, pixelSize, a.colour_wheel, coarsening=1000)
-
-
 plt.show()
+
+#calculate ordering from q tensor
+#change intensity threshold as most of image for synthetic data is dark 
+qtensorsInfo=dc.calculateQTensor(cells, kernelSize*2, intensityThreshold=0.01)
+#print(qtensorsInfo, 'qtensorInfo')
+
+#create the interactive ordering heatmap
+dc.create_nematicOrderingTensor_heatmap_interactive(a.masked, cells, qtensorsInfo, kernelSize, pixelSize, a.colour_wheel, coarsening=1000)
